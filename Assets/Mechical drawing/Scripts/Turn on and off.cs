c1bbc8e2d86d60a7744bd0291e2bfcbcf3e385b8
Kano Yang <yangten@shernet.sheridancollege.ca>
2025-01-28 18:09:14 -0500

    able to turn screen on and off.

--- Assets/Mechical drawing/Scripts/Turn on and off.cs
+++ Assets/Mechical drawing/Scripts/Turn on and off.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class Turnonandoff : MonoBehaviour
 {
+    public bool isGameOn = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            isGameOn = !isGameOn;
+        }
+
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        Vector3 pos = transform.position;
+
+        if(isGameOn == true)
+        {
+            pos.z = 3;
+
+            
+        }
+        else
+        {
+            pos.z = 0;
+        }
+        transform.position = pos;
     }
 }
