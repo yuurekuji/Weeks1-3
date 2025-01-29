28b9c8fb877513507e0832bb5e2e2b7d65ab65f0
yuurekuji <168787708+yuurekuji@users.noreply.github.com>
2025-01-28 22:18:52 -0500

    Fixed the issue where the inputs for the mouse was not being detected. Added comments to the code.

--- Assets/Mechical drawing/Scripts/Turn on and off.cs
+++ Assets/Mechical drawing/Scripts/Turn on and off.cs
@@ -10,21 +10,23 @@ public class Turnonandoff : MonoBehaviour
     void Start()
     {
 
-
-        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
-        {
-            isGameOn = !isGameOn;
-        }
-
-       
     }
 
     // Update is called once per frame
     void Update()
     {
+        //this conditional statement is to check if the player right or left clicks with the mouse, if they do then the IsGameOn boolean will change to the opposite and trigger the if statement below to change z value
+        
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            isGameOn = !isGameOn;
+        }
 
+        //we use vector 3 as I wish to configure the Z value of the positions.
         Vector3 pos = transform.position;
 
+        // this if statement checks if the boolean for isGameOn is true, if it is then it will change the pos of the Z value to 3 ( a value behind all other layers ) 
+        // by using layers with Z value, it can create the illusion of the object dissapearing without using the destroy function. 
         if(isGameOn == true)
         {
             pos.z = 3;
